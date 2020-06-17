d328360c6074a872bcc88ccdc460ce5c29aef547
huangbangyi2019 <1094476088@qq.com>
2020-06-17 11:37:04 +0800

    谭思敏的操作

--- team-project/src/com/nnxy/team3/tansimin.java
+++ team-project/src/com/nnxy/team3/tansimin.java
@@ -2,16 +2,7 @@ package com.nnxy.team3;
 
 public class tansimin {
 	
-	private String name;
 	private int id;
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
 	
 	public int getId() {
 		return id;
