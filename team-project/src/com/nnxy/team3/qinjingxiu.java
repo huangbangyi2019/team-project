641d14d5f2d19f62dbbfe8ea84ae0a65e5ee4901
huangbangyi2019 <1094476088@qq.com>
2020-06-17 11:05:07 +0800

    将本地代码添加到本地仓库108

--- team-project/src/com/nnxy/team3/qinjingxiu.java
+++ team-project/src/com/nnxy/team3/qinjingxiu.java
@@ -5,6 +5,17 @@ public class qinjingxiu {
 	
 	private int id;
 	
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public int getId () {
 		
 		return id;
@@ -16,6 +27,5 @@ public class qinjingxiu {
 	}
 	
 	
-	
-	
+
 }
